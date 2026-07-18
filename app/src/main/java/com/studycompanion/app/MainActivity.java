3bae284162c1c14a0d832ca99a0a6392af26b9a8
akhilnetha240512-cell <akhilnetha240512@gmail.com>
2026-07-18 21:08:26 +0530

    Update MainActivity.java

--- app/src/main/java/com/studycompanion/app/MainActivity.java
+++ app/src/main/java/com/studycompanion/app/MainActivity.java
@@ -1,3 +1,16 @@
-AdView mAdView = findViewById(R.id.adView);
-AdRequest adRequest = new AdRequest.Builder().build();
-mAdView.loadAd(adRequest);
+package com.studycompanion.app;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+public class MainActivity extends AppCompatActivity {
+    @Override protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(this, {});
+        AdView adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+    }
+}
