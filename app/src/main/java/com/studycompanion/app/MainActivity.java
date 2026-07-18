d54e0abad1eeaec45307f8d0160cfd2c03e74761
akhilnetha240512-cell <akhilnetha240512@gmail.com>
2026-07-18 21:05:14 +0530

    Refactor ad loading in MainActivity
    Refactored ad loading code to use mAdView.

--- app/src/main/java/com/studycompanion/app/MainActivity.java
+++ app/src/main/java/com/studycompanion/app/MainActivity.java
@@ -1,27 +1,3 @@
-package com.studycompanion.app;
-import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.ads.*;
-public class MainActivity extends AppCompatActivity {
-    private WebView webView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        MobileAds.initialize(this, initializationStatus -> {});
-        AdView adView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);
-        webView = findViewById(R.id.webview);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://akhilnetha240512-cell.github.io/study-companion-app/");
-    }
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) webView.goBack();
-        else super.onBackPressed();
-    }
-}
+AdView mAdView = findViewById(R.id.adView);
+AdRequest adRequest = new AdRequest.Builder().build();
+mAdView.loadAd(adRequest);
