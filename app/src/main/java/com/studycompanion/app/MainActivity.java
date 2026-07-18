875c5d9874f8ffb88014dfa7bae0e5c2123ffc51
akhilnetha240512-cell <akhilnetha240512@gmail.com>
2026-07-18 21:19:53 +0530

    Update AdMob initialization in MainActivity

--- app/src/main/java/com/studycompanion/app/MainActivity.java
+++ app/src/main/java/com/studycompanion/app/MainActivity.java
@@ -1,16 +1,1 @@
-package com.studycompanion.app;
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-public class MainActivity extends AppCompatActivity {
-    @Override protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        MobileAds.initialize(this, {});
-        AdView adView = findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        adView.loadAd(adRequest);
-    }
-}
+android:value="ca-app-pub-7311652007015024∼9628739277
